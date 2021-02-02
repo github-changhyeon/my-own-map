ade788448ef5373f6a027a33d6ea3b73d5b5638c
김순석 <chaneli1194@naver.com>
2021-02-02 18:53:23 +0900

    [ADD] Follow.vue

---
+++ front/src/components/user/Follow.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <span>
+        <v-icon>mdi-account-plus</v-icon>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {
+  name: 'Follow',
+  props: {
+    users: {
+      type: Array,
+      required: true,
+    },
+    remove: {
+      type: Function,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+  created() {
+    // axios.get()
+  },
+};
+</script>
+
+<style></style>
--- front/src/components/user/UserInfo.vue
+++ front/src/components/user/UserInfo.vue
@@ -1,29 +1,72 @@
 <template>
   <div>
     User Info
+
     <div>
       <v-avatar>
         <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
       </v-avatar>
+      <div>
+        팔로잉
+        <Follow :users="followingList" :followBtn="followBtn" />
+      </div>
+      <div>
+        팔로워
+        <Follow :users="followerList" :followBtn="followBtn" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 // import axios from 'axios';
+import Follow from '@/components/user/Follow';
 
 export default {
   name: 'UserInfo',
+  components: {
+    Follow,
+  },
   props: {
     profile: Object,
   },
   data() {
     return {
       myImg: '',
+      followingList: [
+        {
+          id: 1,
+          nickname: 'Al',
+        },
+        {
+          id: 2,
+          nickname: 'AgFe',
+        },
+        {
+          id: 3,
+          nickname: 'bummy',
+        },
+      ],
+      followerList: [
+        {
+          id: 1,
+          nickname: 'Al',
+        },
+        {
+          id: 2,
+          nickname: 'AgFe',
+        },
+        {
+          id: 3,
+          nickname: 'bummy',
+        },
+      ],
     };
   },
   methods: {},
-  created() {},
+  created() {
+    // axios.post(`http://localhost:8080/users/follow/${userId}/`);
+  },
 };
 </script>
 
