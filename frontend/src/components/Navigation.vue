e62f3ff32c225d82477701c828c4a2ac5d4d7ff5
multicampus <sbseok12@gmail.com>
2021-02-11 03:29:49 +0900

    [ADD] favicon.ico and icons on nav bar

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -1,18 +1,43 @@
 <template>
-  <v-bottom-navigation
-    :value="value"
-    color="primary"
-    style="position: fixed;bottom: 0px; z-index: 2; display: flex; justify-content: space-around; background-color:white;"
-    class="nav"
-  >
+  <v-bottom-navigation :value="value" color="primary" style="position: fixed;bottom: 0px; z-index: 2; display: flex; justify-content: space-around; background-color:white;" class="nav">
+    <!-- <router-link :to="`/main/${this.uid}`" style="textDecoration:none;">
+    <v-btn @click="$router.push(`/main/${uid}`)" style="background-color:white; margin-top:8px;">
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+    </router-link>
+    <router-link to="/newsfeed" style="textDecoration:none;">
+    <v-btn @click="$router.push(`/newsfeed`)" style="background-color:white; margin-top:8px;">
+      <v-icon>mdi-newspaper-variant</v-icon>
+    </v-btn>
+    </router-link>
+    <router-link :to="`/articles/create`" style="textDecoration:none;">
+    <v-btn @click="$router.push(`/articles/create`)" style="background-color:white; margin-top:8px;">
+      <img width="40px" height="44px" src="@/assets/MOM_Icon.png" />
+    </v-btn>
+    </router-link>
+    <router-link :to="`/articles/create`" style="textDecoration:none;">
+    <v-btn @click="$router.push(`/articles/create`)" style="background-color:white; margin-top:8px;">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+    </router-link>
+    <router-link :to="`/users/${this.uid}`" style="textDecoration:none;">
+    <v-btn @click="$router.push(`/user/${uid}`)" style="background-color:white; margin-top:8px;">
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+    </router-link> -->
     <router-link :to="`/main/${this.uid}`" style="textDecoration:none;">
       <v-btn style="background-color:white; margin-top:8px;">
         <v-icon>mdi-home</v-icon>
       </v-btn>
     </router-link>
-    <router-link to='/newsfeed' style="textDecoration:none;">
+    <router-link to="/newsfeed" style="textDecoration:none;">
       <v-btn style="background-color:white; margin-top:8px;">
         <v-icon>mdi-newspaper-variant</v-icon>
+      </v-btn>
+    </router-link>
+    <router-link :to="`/articles/create`" style="textDecoration:none;">
+      <v-btn style="background-color:white; margin-top:8px;">
+        <img width="40px" height="44px" src="@/assets/MOM_Icon.png" />
       </v-btn>
     </router-link>
     <router-link :to="`/articles/create`" style="textDecoration:none;">
@@ -30,43 +55,40 @@
 
 <script>
 import jwt_decode from 'jwt-decode';
-  export default {
-    data: () => ({ 
-        value: 1,
-        uid: 0, 
-        // uid:jwt_decode(localStorage.getItem('jwt')), 
-      }),
-    methods: {
-      // goToNewsFeed() {
-
-      // },
-      // goToMypage() {
-        
-      // },
-      // refreshToken(){
-        
-      // const token = localStorage.getItem('jwt');
-      // this.uid = jwt_decode(token).uid;
-      // }
-    },
-    created(){
-      const token = localStorage.getItem('jwt');
-      if(token !== null && token !== undefined) {
-        this.uid = jwt_decode(token).uid
-      }
-      console.log(token,"uid는", this.uid);
+export default {
+  data: () => ({
+    value: 1,
+    uid: 0,
+    // uid:jwt_decode(localStorage.getItem('jwt')),
+  }),
+  methods: {
+    // goToNewsFeed() {
+    // },
+    // goToMypage() {
+    // },
+    // refreshToken(){
+    // const token = localStorage.getItem('jwt');
+    // this.uid = jwt_decode(token).uid;
+    // }
+  },
+  created() {
+    const token = localStorage.getItem('jwt');
+    if (token !== null && token !== undefined) {
+      this.uid = jwt_decode(token).uid;
     }
-  }
+    console.log(token, 'uid는', this.uid);
+  },
+};
 </script>
 
 <style>
-  /* .nav{
+/* .nav{
     width: 100vw;
     display: flex;
     justify-content: space-around;
   } */
 
-  /* .button {
+/* .button {
     background-color:white;
   } */
 </style>
