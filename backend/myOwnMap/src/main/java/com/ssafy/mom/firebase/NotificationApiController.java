41ec96e13bf6241e6f22c355383d0830c25aed2d
multicampus <dbsdmscjf304@naver.com>
2021-02-14 22:11:31 +0900

    [FIX] fcm/controller

--- backend/myOwnMap/src/main/java/com/ssafy/mom/firebase/NotificationApiController.java
+++ backend/myOwnMap/src/main/java/com/ssafy/mom/firebase/NotificationApiController.java
@@ -6,7 +6,9 @@ import java.util.concurrent.ExecutionException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.mom.config.jwt.JwtService;
 import com.ssafy.mom.dao.UserDao;
+import com.ssafy.mom.model.BasicResponse;
 import com.ssafy.mom.model.UserDto;
 
 @RestController
@@ -34,13 +37,17 @@ public class NotificationApiController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody String token, HttpServletRequest request) {
+    public Object register(@RequestBody String token, HttpServletRequest request) {
+    	final BasicResponse result = new BasicResponse();
         notificationService.register(jwtService.getUserUid(), token);
-        return ResponseEntity.ok().build();
+        result.status=true;
+        result.message="토큰 생성완료";
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
     
     @PostMapping("/push")
-    private void createReceiveNotification(int receiverUid,HttpServletRequest request) throws InterruptedException, ExecutionException {
+    private Object createReceiveNotification(int receiverUid,HttpServletRequest request) throws InterruptedException, ExecutionException {
+    	final BasicResponse result = new BasicResponse();
     	Optional<UserDto> receiver = userDao.findByUid(receiverUid);
         if (receiver.get().isLogin()) {
             NotificationRequest notificationRequest = NotificationRequest.builder()
@@ -49,7 +56,23 @@ public class NotificationApiController {
                 .message("확인")
                 .build();
             notificationService.sendNotification(notificationRequest);
+            result.status=true;
+            result.message="푸쉬완료.";
+            return new ResponseEntity<>(result, HttpStatus.OK);
         }
+        result.status=true;
+        result.message="상대가 로그인중이 아닙니다.";
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/logout")
+    public Object logout(HttpServletRequest http) {
+    	final BasicResponse result = new BasicResponse();
+        notificationService.deleteToken(jwtService.getUserUid());
+        
+        result.status=true;
+        result.message="토큰 삭제완료.";
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
