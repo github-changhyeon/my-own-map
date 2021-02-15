406613948687828008287df938321d094c79300a
multicampus <dbsdmscjf304@naver.com>
2021-02-15 18:51:56 +0900

    [ADD] /fcm/history

--- backend/myOwnMap/src/main/java/com/ssafy/mom/firebase/NotificationApiController.java
+++ backend/myOwnMap/src/main/java/com/ssafy/mom/firebase/NotificationApiController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.mom.config.jwt.JwtService;
 import com.ssafy.mom.dao.ArticleDao;
 import com.ssafy.mom.dao.FavoriteDao;
+import com.ssafy.mom.dao.HistoryDao;
 import com.ssafy.mom.dao.UserDao;
 import com.ssafy.mom.dao.UserFollowDao;
 import com.ssafy.mom.model.ArticleDto;
 import com.ssafy.mom.model.BasicResponse;
+import com.ssafy.mom.model.History;
 import com.ssafy.mom.model.UserDto;
 import com.ssafy.mom.model.UserFavoriteDto;
 import com.ssafy.mom.model.UserFollow;
@@ -56,6 +59,9 @@ public class NotificationApiController {
 
 	@Autowired
 	ArticleDao articleDao;
+	
+	@Autowired
+	HistoryDao historyDao;
 
 	private final NotificationService notificationService;
 
@@ -72,7 +78,43 @@ public class NotificationApiController {
 		result.message = "토큰 생성완료";
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
-
+	
+	@GetMapping("/history")
+	public Object retrieveHistory(HttpServletRequest request) {
+		final BasicResponse result = new BasicResponse();
+		int myUid = jwtService.getUserUid();
+		Optional<UserDto> user = userDao.findByUid(myUid);
+		if(!user.isPresent()) {
+			result.status=false;
+			result.message="/fcm/history : 유저가 없어";
+			return new ResponseEntity<>(result,HttpStatus.OK);
+		}
+		List<History> historyList = historyDao.findAllByUserTo(user.get());
+		System.out.println(historyList.toString()+" /fcm/history-리스트 잘 가져옴?");
+		result.status = true;
+		result.message = "토큰 생성완료";
+		result.object= historyList;
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@GetMapping("/makeZero")
+	public Object makeZero(HttpServletRequest request) {
+		final BasicResponse result = new BasicResponse();
+		int myUid = jwtService.getUserUid();
+		Optional<UserDto> user = userDao.findByUid(myUid);
+		if(!user.isPresent()) {
+			result.status=false;
+			result.message="/fcm/makeZero - 유저가 없음";
+			return new ResponseEntity<>(result,HttpStatus.OK);
+		}
+		UserDto userEntity = user.get();
+		userEntity.setNotificationCheck(0);
+		userDao.save(userEntity);
+		result.status=true;
+		result.message="/fcm/makeZero - NotificationCheck 0으로 만듬!";
+		return new ResponseEntity<>(result,HttpStatus.OK);
+	}
+	
 	@PostMapping("/push")
 	private Object createReceiveNotification(@RequestBody Map<String, String> mapDto, HttpServletRequest request)
 			throws InterruptedException, ExecutionException {
@@ -82,30 +124,58 @@ public class NotificationApiController {
 		String message = mapDto.get("message");
 		Optional<UserDto> sender = userDao.findByUid(myUid);
 		Optional<UserDto> receiver = userDao.findByUid(receiverUid);
-
+		History history;
+		
 		// 팔로우
 		if (message == "FOLLOW") {
 			Optional<UserFollow> userFollow = userFollowDao.findByUserFromAndUserTo(sender.get(), receiver.get());
 			// 팔로우중이라면
 			if (userFollow.isPresent()) {
 				// 취소푸쉬 xx
-				result.status = true;
+				result.status = false;
 				result.message = "이미 팔로우 중이라 팔로우취소가 되니 푸쉬알림x.";
 				return new ResponseEntity<>(result, HttpStatus.OK);
 			}
+			
 			sendPush(message, sender.get(), receiver.get());
 			// 히스토리저장
+			history = History.builder()
+					.userFrom(sender.get())
+					.userTo(receiver.get())
+					.state(message)
+					.build();
+			historyDao.save(history);
 			result.status = true;
 			result.message = "팔로우푸쉬(히스토리) 완.";
 			return new ResponseEntity<>(result, HttpStatus.OK);
 		}
 		// 댓글
 		else if (message == "COMMENT") {
-			// 로그인중이라면 푸쉬
+			//해당 댓글의 게시글 찾기
+			int articleNo = Integer.parseInt(mapDto.get("articleNo"));
+			Optional<ArticleDto> article = articleDao.findByArticleNo(articleNo);
+			//게시글이 없다면 오류 출력
+			if(!article.isPresent()) {
+				System.out.println("오류!오류! : sendPush-게시글없음");
+				result.status = false;
+				result.message = "오류!오류! 너는 삼류 : sendPush.";
+				return new ResponseEntity<>(result, HttpStatus.OK);
+			}
+			
+			// 로그인중이라면 푸쉬알림
 			if (receiver.get().isLogin()) {
 				sendPush(message, sender.get(), receiver.get());
 			}
-			// 히스토리저장
+			
+			history = History.builder()
+					.userFrom(sender.get())
+					.userTo(receiver.get())
+					.state(message)
+					.articleDto(article.get())
+					.build();
+			historyDao.save(history);
+			result.status = true;
+			result.message = "댓글푸쉬(히스토리) 완.";
 			return new ResponseEntity<>(result, HttpStatus.OK);
 		}
 		// 찜하기
@@ -116,13 +186,20 @@ public class NotificationApiController {
 			// 찜한상태라면
 			if (isFavorite.isPresent()) {
 				// 취소푸쉬는 xx
-				result.status = true;
+				result.status = false;
 				result.message = "이미 찜하기 중이라 찜하기취소가 되니 푸쉬알림x.";
 				return new ResponseEntity<>(result, HttpStatus.OK);
 			}
 
 			sendPush(message, sender.get(), receiver.get());
 			// 히스토리저장
+			history = History.builder()
+					.userFrom(sender.get())
+					.userTo(receiver.get())
+					.state(message)
+					.articleDto(article.get())
+					.build();
+			historyDao.save(history);
 			result.status = true;
 			result.message = "찜하기푸쉬(히스토리)완.";
 			return new ResponseEntity<>(result, HttpStatus.OK);
@@ -139,7 +216,7 @@ public class NotificationApiController {
 	}
 
 	@DeleteMapping("/logout")
-	public Object logout(HttpServletRequest http) {
+	public Object logout(HttpServletRequest request) {
 		final BasicResponse result = new BasicResponse();
 		int myUid = jwtService.getUserUid();
 		notificationService.deleteToken(myUid);
@@ -152,10 +229,16 @@ public class NotificationApiController {
 	}
 
 	public void sendPush(String message, UserDto sender, UserDto receiver) {
-		if (message == "FOLLOW") {
+		//푸쉬하면 히스토리 갯수 추가!
+		UserDto user = receiver;
+		user.setNotificationCheck(user.getNotificationCheck()+1);
+		userDao.save(user);
+		
+		//message 조건 처리
+		if (message == "FOLLOW") {			
 			NotificationRequest notificationRequest = NotificationRequest.builder().title("Follow")
 					.token(notificationService.getToken(receiver.getUid()))
-					.message(NotificationType.FOLLOW_RECEIVED.generateNotificationMessage(sender, receiver)).build();
+					.message(NotificationType.FOLLOW_RECEIVED.generateNotificationMessage(sender, receiver)).build();			
 			notificationService.sendNotification(notificationRequest);
 		} else if (message == "LIKE") {
 			NotificationRequest notificationRequest = NotificationRequest.builder().title("LIKE")
---
+++ backend/myOwnMap/src/main/java/com/ssafy/mom/model/History.java
@@ -0,0 +1,74 @@
+package com.ssafy.mom.model;
+
+import java.time.LocalDateTime;
+
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity(name = "history")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class History {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int historyNo;
+	
+	@ManyToOne(cascade = CascadeType.DETACH)
+	@JoinColumn(name = "uid") // 외래키의 주인
+	private UserDto userFrom;
+	
+	@ManyToOne(cascade = CascadeType.DETACH)
+	@JoinColumn(name = "uid") // 외래키의 주인
+	private UserDto userTo;
+	
+	@ManyToOne(cascade = CascadeType.DETACH)
+	@JoinColumn(name = "articleNo") // 외래키의 주인
+	private ArticleDto articleDto;
+	
+	//팔로우, 찜, 댓글
+	private String state;
+	
+	@CreationTimestamp
+	private LocalDateTime regiTime;
+	
+	@Builder
+	public History(UserDto userFrom, UserDto userTo, ArticleDto articleDto, String state) {
+		super();
+		this.userFrom = userFrom;
+		this.userTo = userTo;
+		this.articleDto = articleDto;
+		this.state = state;
+	}
+	
+	@Builder
+	public History(UserDto userFrom, UserDto userTo, String state) {
+		super();
+		this.userFrom = userFrom;
+		this.userTo = userTo;
+		this.state = state;
+	}
+	
+	
+	
+	
+}
