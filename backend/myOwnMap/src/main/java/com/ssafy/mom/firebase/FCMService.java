7a7cf3aa67658c871f263565c62e648ff41c4ead
Kim Changhyeon <ckdhyeon95@naver.com>
2021-02-16 14:45:00 +0900

    [FIX] FCM SERVICE

--- backend/myOwnMap/src/main/java/com/ssafy/mom/firebase/FCMService.java
+++ backend/myOwnMap/src/main/java/com/ssafy/mom/firebase/FCMService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import com.google.firebase.messaging.WebpushConfig;
 import com.google.firebase.messaging.WebpushNotification;
 
@@ -17,12 +18,17 @@ public class FCMService {
     private static final Logger logger = LoggerFactory.getLogger(FCMService.class);
 
     public void send(final NotificationRequest notificationRequest) throws InterruptedException, ExecutionException {
-        Message message = Message.builder()
+//        Message message = Message.builder()
+//                .setToken(notificationRequest.getToken())
+//                .setWebpushConfig(WebpushConfig.builder().putHeader("ttl", "300")
+//                        .setNotification(new WebpushNotification(notificationRequest.getTitle(),
+//                                notificationRequest.getMessage()))
+//                        .build())
+//                .build();
+    	
+    	Message message = Message.builder()
                 .setToken(notificationRequest.getToken())
-                .setWebpushConfig(WebpushConfig.builder().putHeader("ttl", "300")
-                        .setNotification(new WebpushNotification(notificationRequest.getTitle(),
-                                notificationRequest.getMessage()))
-                        .build())
+                .setNotification(new Notification(notificationRequest.getTitle(), notificationRequest.getMessage()))
                 .build();
 
         String response = FirebaseMessaging.getInstance().sendAsync(message).get();
